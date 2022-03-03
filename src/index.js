6e613b613082a292b402fbd4bdbba30ed4ab9988
Daniel <dani.erik18ap@gmail.com>
2022-03-03 12:28:29 -0600

    repairing the endpoints

--- src/index.js
+++ src/index.js
@@ -8,19 +8,29 @@ app.listen(PORT, () => {
 });
 app.post('/image/transform', (req, res) => {
     let authorization = req.headers.authorization;
-    let separateStrings = authorization.split(' ')[1]
-    let bufferAuth = new Buffer.from(separateStrings, 'base64');
-    let decodedString = bufferAuth.toString('ascii');
-    let decodedStringArray = decodedString.split(':');
-    let authUser = decodedStringArray[0];
-    let authPassword = decodedStringArray[1];
-    let bufferLiink = new Buffer.from(req.body.link);
-    let base64data = bufferLiink.toString('base64');
+
     if (!authorization) {
         return res.status(401).send('Unauthorized');
     } else {
-        if (authUser === 'test' && authPassword === 'password') {
-            return res.status(201).json({ "base64": base64data });
+        let separateStrings = authorization.split(' ')[1]
+        let bufferAuth = new Buffer.from(separateStrings, 'base64');
+        let decodedString = bufferAuth.toString('ascii');
+        let decodedStringArray = decodedString.split(':');
+        let authUser = decodedStringArray[0];
+        let authPassword = decodedStringArray[1];
+
+        if (authUser === 'test' && authPassword === '123456') {
+            if (Object.keys(req.body).length === 0) {
+                return res.status(400).json({
+                    error: 'Bad request',
+                    message: 'The request body must not be empty'
+                });
+            } else {
+                let bufferLiink = new Buffer.from(req.body.link);
+                console.log("hola");
+                let base64data = bufferLiink.toString('base64');
+                return res.status(201).json({ "base64": base64data });
+            }
         } else {
             return res.status(401).send('Unauthorized');
         }
@@ -34,22 +44,19 @@ app.post('/object/clean', (req, res) => {
             message: 'The request body must not be empty'
         });
     } else {
-        // iterate over the object and check if the key is a null
-        Object.keys(req.body).forEach(key => {
-            if (req.body[key] === null) {
-                delete req.body[key];
-            } else {
-                // if the value is an object, iterate over it and check if the key is a null
-                if (typeof req.body[key] === "object") {
-                    Object.keys(req.body[key]).forEach(key2 => {
-                        console.log(key2);
-                        if (req.body[key][key2] === null) {
-                            delete req.body[key][key2];
-                        }
-                    });
+        let obj = req.body;
+        function removeNull (object) {
+            let objectNew = {};
+            Object.entries(object).forEach(([key, value]) => {
+                if (value === Object(value)) {
+                    objectNew[key] = removeNull(value);
+                } else if (value != null) {
+                    objectNew[key] = object[key];
                 }
-            }
-        });
-        return res.status(201).json(req.body);
+            });
+            return objectNew
+
+        }
+        return res.status(201).json(removeNull(obj));
     }
 })
